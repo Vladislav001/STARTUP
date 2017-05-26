33d4ab9dc8dfe3a75570295625df3f3ea3f65a4a
Hugo61 <vyacheslav8g@yandex.ru>
2017-05-26 03:35:36 +0300

    фикс обработки информации юзеров для плиток(render() теперь в колбеке поиска по монге - .find())

--- routes/personalArea.js
+++ routes/personalArea.js
@@ -2,24 +2,24 @@ var user = require('../models/user').User;
 var HttpError = require('../error').HttpError;
 var AuthError = require('../models/user').AuthError;
 
-var links = [];  // массив в котором будут храниться сформированные адреса профилей юзеров
-var usernames = []; // хранит имена юзеров
-
-user.find({}, function(err, doc) { 
-	for(var i = 0; i < doc.length; i++) {
-
-		links.push("/id" + doc[i]._id);  // добавляем в конец links очередную id
-		usernames.push(doc[i].username);
-	}
-});
-
-
-
-
-
 exports.get = function(req, res) {
 
-  res.render('personalArea', {links: links, usernames: usernames});
+	var links = [];  // массив в котором будут храниться сформированные адреса профилей юзеров
+	var usernames = []; // хранит имена юзеров
 
+	user.find({}, function(err, doc, next) { 
+		for(var i = 0; i < doc.length; i++) {
+
+			links.push("/id" + doc[i]._id);  // добавляем в конец links очередную id
+			usernames.push(doc[i].username);
+
+
+		}
+		
+		res.render('personalArea', {links: links, usernames: usernames});
+		
+	});
+
+	
    
 };
